9e33460b322317642c80f46013fc53166c734c72
IndrePet <indrepetrausku@gmail.com>
2022-11-14 22:16:58 +0200

    Setting up DB

--- app.js
+++ app.js
@@ -9,6 +9,15 @@ app.set("view engine", "ejs");
 app.use(bodyParser.url, encoded());
 app.use(express.static("public"));
 
+mongoose.connect("mongodb://127.0.0.1:27017/wikiDB");
+
+const articleSchema = new mongoose.Schema({
+  title: String,
+  content: String,
+});
+
+const Article = new mongoose.model("Article", articleSchema);
+
 app.listen(3000, function () {
   console.log("Server started on port 3000");
 });
