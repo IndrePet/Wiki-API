e3a27acb330c47dceb8d71aac654bb270f1f3ac1
IndrePet <indrepetrausku@gmail.com>
2022-11-14 22:44:37 +0200

    POST

--- app.js
+++ app.js
@@ -18,15 +18,31 @@ const articleSchema = new mongoose.Schema({
 
 const Article = new mongoose.model("Article", articleSchema);
 
-app.route("/articles").get((req, res) => {
-  Article.find((err, foundArticles) => {
-    if (!err) {
-      res.send(foundArticles);
-    } else {
-      res.send(err);
-    }
+app
+  .route("/articles")
+  .get((req, res) => {
+    Article.find((err, foundArticles) => {
+      if (!err) {
+        res.send(foundArticles);
+      } else {
+        res.send(err);
+      }
+    });
+  })
+  .post((req, res) => {
+    const newArticle = new Article({
+      title: req.body.title,
+      content: req.body.content,
+    });
+
+    newArticle.save(err => {
+      if (err) {
+        res.send(err);
+      } else {
+        res.send("Successfully added a new article");
+      }
+    });
   });
-});
 
 app.listen(3000, function () {
   console.log("Server started on port 3000");
