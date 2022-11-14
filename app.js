4bfc69f98c421e71e9c25c1178d21835dc531a8f
IndrePet <indrepetrausku@gmail.com>
2022-11-14 22:26:32 +0200

    GET All Articles

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const app = express();
 
 app.set("view engine", "ejs");
 
-app.use(bodyParser.url, encoded());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 mongoose.connect("mongodb://127.0.0.1:27017/wikiDB");
@@ -17,6 +17,16 @@ const articleSchema = new mongoose.Schema({
 });
 
 const Article = new mongoose.model("Article", articleSchema);
+
+app.route("/articles").get((req, res) => {
+  Article.find((err, foundArticles) => {
+    if (!err) {
+      res.send(foundArticles);
+    } else {
+      res.send(err);
+    }
+  });
+});
 
 app.listen(3000, function () {
   console.log("Server started on port 3000");
